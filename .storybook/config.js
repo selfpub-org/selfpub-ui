5399cba6e7870a9cc2ee1ae233b182943a7c44a1
Ilya Reshetnikov <devxom@gmail.com>
2018-07-31 16:53:08 +0300

    Update package and config storybook
    Signed-off-by: Ilya Reshetnikov <devxom@gmail.com>

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,9 @@
 import { configure } from '@storybook/react';
 
+const req = require.context('../components', true, /\.stories\.js$/);
+
 function loadStories() {
-  require('../stories/index.js');
-  // You can require as many stories as you need.
+  req.keys().forEach((filename) => req(filename))
 }
 
 configure(loadStories, module);
