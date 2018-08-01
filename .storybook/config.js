9f4e47d1dc433eddb83b9408ff61629f26f8b1a9
Ilya Reshetnikov <devxom@gmail.com>
2018-08-01 18:14:40 +0300

    Update configuration
    Signed-off-by: Ilya Reshetnikov <devxom@gmail.com>

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,13 @@
-import { configure } from '@storybook/react';
+/* global require */
+/* global module */
+import { configure, addDecorator } from '@storybook/react';
+import { withKnobs } from "@storybook/addon-knobs";
 
-const req = require.context('../components', true, /\.stories\.js$/);
+// Add the `withKnobs` decorator to add knobs support to your stories.
+addDecorator(withKnobs);
 
 function loadStories() {
+const req = require.context('../components', true, /\.stories\.js$/);
   req.keys().forEach((filename) => req(filename))
 }
 
