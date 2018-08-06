f7917b5929c343f7a81e5dbaa48ffa181af4cc9f
Ilya Reshetnikov <devxom@gmail.com>
2018-08-06 19:14:46 +0300

    Update storybook config
    Signed-off-by: Ilya Reshetnikov <devxom@gmail.com>

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,12 +2,21 @@
 /* global module */
 import { configure, addDecorator } from '@storybook/react';
 import { withKnobs } from "@storybook/addon-knobs";
+import backgrounds from '@storybook/addon-backgrounds';
 
-// Add the `withKnobs` decorator to add knobs support to your __stories__.
 addDecorator(withKnobs);
 
+addDecorator(
+  backgrounds([
+    { name: 'White', value: '#ffffff', default: true },
+    { name: 'Main', value: '#efeee9' },
+    { name: 'Footer, header', value: '#3b393f' },
+    { name: 'Profile', value: '#efefef' },
+  ])
+);
+
 function loadStories() {
-const req = require.context('../components', true, /\.stories\.js$/);
+const req = require.context('../src/components', true, /\.stories\.js$/);
   req.keys().forEach((filename) => req(filename))
 }
 
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,23 @@
 /* global module */
 /* global __dirname */
+const path = require("path");
+
 module.exports = {
   resolve: {
     modules: [
       'components',
       'node_modules',
     ],
+    alias: {
+      "@selfpub-ui": path.resolve("src"),
+    },
   },
+  module: {
+    rules: [
+      {
+        test: /\.(woff2?|ttf|eot|svg|jpe?g|png|gif)$/,
+        use: ["file-loader?name=[path][name].[hash:8].[ext]"],
+      },
+    ]
+  }
 };
