2f922100f5340539c783ec387917b5305b9c751e
Ilya Reshetnikov <devxom@gmail.com>
2018-08-13 11:58:47 +0300

    Update dependencies
     - replace eol-dep
     - Remove un need peer-dep
    
    Signed-off-by: Ilya Reshetnikov <devxom@gmail.com>

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -3,6 +3,6 @@ import '@storybook/addon-backgrounds/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-options/register';
 import '@storybook/addon-viewport/register'
-import 'storybook-addon-jest/register';
-import 'storybook-addon-jest/styles';
+import '@storybook/addon-jest/register';
+import '@storybook/addon-jest/styles';
 
--- .storybook/withTest.js
+++ .storybook/withTest.js
@@ -1,5 +1,5 @@
 import jestTestResults from '../.out/jest-test-results.json';
-import withTests from 'storybook-addon-jest';
+import { withTests } from '@storybook/addon-jest';
 
 export default withTests(jestTestResults, {
   filesExt: '.test.js',
