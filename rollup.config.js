3b6a9a5c7c9bdb758d7b2ef3e85e195f297c25d1
Ilya Reshetnikov <ilya.reshetnikov@devxom.tech>
2019-08-28 17:57:02 +0300

    fix: remove using vulnerable dependecie
    Signed-off-by: Ilya Reshetnikov <ilya.reshetnikov@devxom.tech>

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,11 @@
 import nodeResolve from "rollup-plugin-node-resolve";
 import commonJs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
-import builtins from "rollup-plugin-node-builtins";
 import embedCSS from "rollup-plugin-embed-css";
 import replace from "rollup-plugin-replace";
 import { terser } from "rollup-plugin-terser";
 
 import pkg from "./package.json";
-
-const peerDependencies = pkg.peerDependencies;
 
 const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : "production";
 const isProduction = NODE_ENV === "production";
@@ -49,8 +46,44 @@ const plugins = [
       "node_modules/esrever/esrever.js": ["reverse"],
     },
   }),
-  builtins(),
-  isProduction && terser(),
+  isProduction &&
+    terser({
+      // Use multi-process parallel running to improve the build speed
+      // Default number of concurrent runs: os.cpus().length - 1
+      // reduce count for performance on stage
+      numWorkers: 4,
+      parse: {
+        // we want terser to parse ecma 8 code. However, we don't want it
+        // to apply any minfication steps that turns valid ecma 5 code
+        // into invalid ecma 5 code. This is why the 'compress' and 'output'
+        // sections only apply transformations that are ecma 5 safe
+        ecma: 8,
+      },
+      compress: {
+        ecma: 5,
+        warnings: false,
+        // Disabled because of an issue with Uglify breaking seemingly valid code:
+        // https://github.com/facebook/create-react-app/issues/2376
+        // Pending further investigation:
+        // https://github.com/mishoo/UglifyJS2/issues/2011
+        comparisons: false,
+        // Disabled because of an issue with Terser breaking valid code:
+        // https://github.com/facebook/create-react-app/issues/5250
+        // Pending futher investigation:
+        // https://github.com/terser-js/terser/issues/120
+        inline: 2,
+      },
+      mangle: {
+        safari10: true,
+      },
+      output: {
+        ecma: 5,
+        comments: false,
+        // Turned on because emoji and regex is not minified properly using default
+        // https://github.com/facebook/create-react-app/issues/2488
+        ascii_only: true,
+      },
+    }),
   replace({
     "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
   }),
@@ -60,7 +93,7 @@ export default [
   // CommonJS (for Node) and ES module (for bundlers) build.
   {
     input: "src/components/index.js",
-    external: peerDependencies,
+    external: ["react", "react-dom", "styled-components"],
     output: [
       {
         name: pkg.name,
